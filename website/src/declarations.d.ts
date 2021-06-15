bf7025fb15c14ffe6a8f1e1e75a2716317459a4b
rexwangcc <rexwangcc@gmail.com>
2021-06-14 21:10:53 -0400

    Pave the road for typescript.

---
+++ website/src/declarations.d.ts
@@ -0,0 +1,7 @@
+// This file is used to hold ambient type declarations, as well as type shims
+// for npm module without type declarations, and assets files.
+
+/// <reference types="@docusaurus/module-type-aliases" />
+/// <reference types="@docusaurus/theme-classic" />
+
+declare module '*.gif'
