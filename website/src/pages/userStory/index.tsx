b9f7e2ca6189c3d0304e8ff798643234fb393794
tobebetter <>
2022-04-13 13:32:16 +0800

    modified userstory mobile

--- website/src/pages/userStory/index.tsx
+++ website/src/pages/userStory/index.tsx
@@ -5,7 +5,8 @@ import ManyLine from './img/Line.png';
 import styles from './index.module.scss';
 import ETH from './img/ETH.png';
 import Sand from './img/sand.mp4';
-import './tailwind.css'
+import './tailwind.css';
+import './index.css';
 
 function Article() {
   const manyLineStyle: object = {
@@ -13,6 +14,7 @@ function Article() {
     opacity: '0.2',
     zIndex: '1',
     userSelect: 'none',
+    height: '1074px',
   };
   const contantObj = {
     Goal: [
@@ -40,59 +42,72 @@ function Article() {
       'Further reduced compile time.',
     ],
   };
+  const placeImgStyle = {
+    backgroundImage: image,
+  };
+
   return (
     <div className={styles.box}>
       <div className={styles.placeBox}>
-        <div style={{ marginBottom: '-200px' }}>
-          <img
+        <div>
+          {/* <img
             src={image}
             className={styles.placeImg}
             style={{ width: '100%', marginBottom: '-5px' }}
-          />
+          /> */}
+          <div className={styles.placeImgBg} style={placeImgStyle}></div>
         </div>
         <div className={styles.placeText}>
           <div>Taichi, a perfect programming framework for computer graphics courses</div>
-          <img src={ETH} className={styles.placeImg} style={{ width: '190px', marginLeft: '10px' }} />
-        </div>
-      </div>
-      <div style={{ height: '50px' }}></div>
-      <div className={styles.content}>
-        <div className={styles.para}>
-          <div className={styles.paraPart}>
-            <div style={{ lineHeight: '25px' ,fontWeight:500}}>
-              In Fall 2021, the Computer Graphics Laboratory (CGL) at ETH Zurich offered a course on
-              physically based simulation, which requires students to create a small game or a demo
-              scene using techniques they learned in class.
-            </div>
-          </div>
-          <div className={styles.paraPart} style={{ marginTop: '15px',fontWeight:500 }}>
-            <div className=' md:bg-red-200'>
-              The vast majority of the student groups chose the Taichi programming language for
-              real-time physical simulation.
-            </div>
+          <div className={styles.ethBox}>
+            <img
+              src={ETH}
+              className={styles.placeImg}
+              style={{ width: '190px', marginLeft: '10px' }}
+            />
           </div>
         </div>
       </div>
-      <img src={ManyLine} draggable="false" style={manyLineStyle} />
-      {Object.keys(contantObj).map((item, index) => {
-        return (
-          <div className={styles.content}  key = {`${item}${index}`}>
-            <div className={styles.title}>{item}</div>
-            <div className={styles.para}>
-              {contantObj[item].map((item, index,array) => {
-                return (
-                  <div className={styles.paraPart} key = {`${item}${index}`}>
-                    {
-                      array.length === 1? "" : <img src={Dot} alt="" style={{ marginRight: '5px' }} />
-                    }
-                    <div>{item}</div>
-                  </div>
-                );
-              })}
-              {item === 'Results' ? (
-                <div style={{ marginTop: '20px' }}>
-                  <video src={Sand} width="900" height="421" controls></video>
-                  {/* <iframe
+      <div className={styles.contentBox}>
+        <div className={styles.content}>
+          <div className={styles.para}>
+            <div className={styles.paraPart}>
+              <div style={{ lineHeight: '25px', fontWeight: 500 }}>
+                In Fall 2021, the Computer Graphics Laboratory (CGL) at ETH Zurich offered a course
+                on physically based simulation, which requires students to create a small game or a
+                demo scene using techniques they learned in class.
+              </div>
+            </div>
+            <div className={styles.paraPart} style={{ marginTop: '15px', fontWeight: 500 }}>
+              <div className=" md:bg-red-200">
+                The vast majority of the student groups chose the Taichi programming language for
+                real-time physical simulation.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.manyLines}></div>
+        {Object.keys(contantObj).map((item, index) => {
+          return (
+            <div className={styles.content} key={`${item}${index}`}>
+              <div className={styles.title}>{item}</div>
+              <div className={styles.para}>
+                {contantObj[item].map((item, index, array) => {
+                  return (
+                    <div className={styles.paraPart} key={`${item}${index}`}>
+                      {array.length === 1 ? (
+                        ''
+                      ) : (
+                        <img src={Dot} className={styles.dotImg} alt="" />
+                      )}
+                      <div>{item}</div>
+                    </div>
+                  );
+                })}
+                {item === 'Results' ? (
+                  <div style={{ marginTop: '20px' }}>
+                    <video src={Sand} width="900" height="421" controls></video>
+                    {/* <iframe
                     width="908"
                     height="421"
                     src="https://www.youtube.com/embed/_Uaf5wF9wjo"
@@ -101,14 +116,15 @@ function Article() {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen
                   ></iframe> */}
-                </div>
-              ) : (
-                ''
-              )}
+                  </div>
+                ) : (
+                  ''
+                )}
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
