76a5f2e32fb71a0b37877200155fbdbb8d6fe06e
Justin <62801799+Justinterest@users.noreply.github.com>
2022-06-27 15:56:37 +0800

    blog support multiple language with different dir (#374)
    * blog support multiple language with different dir
    
    * all language preview

--- website/plugins/blog-enhance-plugin/index.js
+++ website/plugins/blog-enhance-plugin/index.js
@@ -1,8 +1,19 @@
 const blogPluginExports = require('@docusaurus/plugin-content-blog');
+const path = require('path')
+const globby = require('globby')
 
 const blogPlugin = blogPluginExports.default;
 
 async function blogPluginEnhanced(context, options) {
+  let pathwithlang = path.join(options.path, context.i18n.currentLocale)
+  console.log(pathwithlang)
+  const pagesFiles = await globby('**/*.{md,mdx}', {
+    cwd: pathwithlang,
+  });
+  if (pagesFiles.length === 0) {
+    pathwithlang = path.join(options.path, context.i18n.defaultLocale)
+  }
+  options.path = pathwithlang
   const blogPluginInstance = await blogPlugin(context, options);
 
   return {
