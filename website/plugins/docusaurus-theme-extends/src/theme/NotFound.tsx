dfae54ff6a5823f2434d79b6a563972ec1770a26
jinfenggt <maxwell.zjf@gmail.com>
2022-08-20 18:24:53 +0800

    add 404 page

--- website/plugins/docusaurus-theme-extends/src/theme/NotFound.tsx
+++ website/plugins/docusaurus-theme-extends/src/theme/NotFound.tsx
@@ -9,6 +9,8 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Translate, {translate} from '@docusaurus/Translate';
 
+import NotfoundSvg from './icons/notfound.svg'
+
 function NotFound(): JSX.Element {
   return (
     <Layout
@@ -16,31 +18,13 @@ function NotFound(): JSX.Element {
         id: 'theme.NotFound.title',
         message: 'Page Not Found',
       })}>
-      <main className="container margin-vert--xl">
-        <div className="row">
-          <div className="col col--6 col--offset-3">
-            <h1 className="hero__title">
-              <Translate
-                id="theme.NotFound.title"
-                description="The title of the 404 page">
-                Page Not Found
-              </Translate>
-            </h1>
-            <p>
-              <Translate
-                id="theme.NotFound.p1"
-                description="The first paragraph of the 404 page">
-                We could not find what you were looking for.
-              </Translate>
-            </p>
-            <p>
-              <Translate
-                id="theme.NotFound.p2"
-                description="The 2nd paragraph of the 404 page">
-                Please contact the owner of the site that linked you to the
-                original URL and let them know their link is broken.
-              </Translate>
-            </p>
+      <main className="container pb-20">
+        <div className="w-full max-w-[960px] mx-auto flex items-center flex-wrap">
+          <NotfoundSvg />
+          <div className='mt-6 w-full desktop:w-fit text-center desktop:text-left desktop:ml-32'>
+            <h1 className='font-bold mb-4'>Page not found.</h1>
+            <div className='mb-8 text-grey-4'>Refresh the page or get back later</div>
+            <button className='rounded-sm border bg-brand-cyan text-white py-2 text-center min-w-[9rem] w-full desktop:w-min' onClick={() => document.location.reload()}>Refresh</button>
           </div>
         </div>
       </main>
