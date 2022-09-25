784805f6fea2ba86466c7b281504cce8aa1f3fb5
jinfenggt <maxwell.zjf@gmail.com>
2022-09-25 20:55:21 +0800

    fix blog scroll

--- website/src/components/LandingPage/index.tsx
+++ website/src/components/LandingPage/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 
-import { ThemeClassNames } from '@docusaurus/theme-common';
+import { ThemeClassNames, useDocsPreferredVersion } from '@docusaurus/theme-common';
 
 import DocPageLayout from '../DocPageLayout';
 
@@ -20,6 +20,9 @@ import type {
   PropSidebarItemCategory,
   PropSidebarItem,
 } from '@docusaurus/plugin-content-docs';
+import {
+  useLatestVersion
+} from '@docusaurus/plugin-content-docs/client'
 import Link from '@docusaurus/Link';
 
 const ArticleCategoryCard: React.FC<{ category: PropSidebarItemCategory }> = ({
@@ -47,123 +50,132 @@ const ArticleCategoryCard: React.FC<{ category: PropSidebarItemCategory }> = ({
   );
 };
 
-export default (props) => {
+const LandingPageView = (props) => {
+  const { savePreferredVersionName } = useDocsPreferredVersion('default');
+  const version = useLatestVersion()
+  useEffect(() => {
+    if (version) {
+      savePreferredVersionName(version.name);
+    }
+  }, [version]);
   return (
-    <Layout
-      wrapperClassName={ThemeClassNames.wrapper.docsPages}
-      pageClassName={ThemeClassNames.page.docsDocPage}
-    >
-      <DocPageLayout sidebar={props.sidebar}>
-        <div className="max-w-[988px] mx-auto">
-          <div className="text-h3 desktop:pb-5 pb-4 font-bold">
-            {translate({
-              id: 'theme.text.dochome',
-              message: 'Doc Home',
-            })}
-          </div>
-          <div className="flex justify-between flex-col desktop:flex-row desktop:space-x-5 desktop:mb-5">
-            <div className="relative flex-1 h-32 overflow-hidden brand-blue-gradients rounded-sm mb-4">
-              <div className="absolute left-[181px] inset-y-0">
-                <NormalUserBg />
-              </div>
-              <Link href='/docs' className="relative flex flex-col justify-between h-full px-5 pt-3 pb-4 text-black cursor-pointer hover:text-white">
-                <div className="text-caption mb-2 text-black">
-                  {translate({
-                    id: 'theme.docs.docHome.userhint',
-                    message: 'Accelerate the Python frontend.',
-                  })}
-                </div>
-                <div className="flex flex-col">
-                  <UserIcon />
-                  <div className="flex justify-between items-center font-bold">
-                    <div className="text-h3">
-                      {translate({
-                        id: 'theme.text.pythonuser',
-                        message: 'Python User',
-                      })}
-                    </div>
-                    <span>
-                      <ArrowRight />
-                    </span>
-                  </div>
-                </div>
-              </Link>
+    <DocPageLayout sidebar={props.sidebar}>
+      <div className="max-w-[988px] mx-auto">
+        <div className="text-h3 desktop:pb-5 pb-4 font-bold">
+          {translate({
+            id: 'theme.text.dochome',
+            message: 'Doc Home',
+          })}
+        </div>
+        <div className="flex justify-between flex-col desktop:flex-row desktop:space-x-5 desktop:mb-5">
+          <div className="relative flex-1 h-32 overflow-hidden brand-blue-gradients rounded-sm mb-4">
+            <div className="absolute left-[181px] inset-y-0">
+              <NormalUserBg />
             </div>
-            <div className="relative flex-1 h-32 overflow-hidden brand-cyan-gradients rounded-sm mb-4">
-              <div className="absolute left-[181px] inset-y-0">
-                <DeveloperUserBg />
+            <Link href='/docs' className="relative flex flex-col justify-between h-full px-5 pt-3 pb-4 text-black cursor-pointer hover:text-white">
+              <div className="text-caption mb-2 text-black">
+                {translate({
+                  id: 'theme.docs.docHome.userhint',
+                  message: 'Accelerate the Python frontend.',
+                })}
               </div>
-              <Link
-                href="/docs/ndarray_android"
-                className="relative flex flex-col justify-between h-full px-5 pt-3 pb-4 text-black cursor-pointer hover:text-white"
-              >
-                <div className="text-caption  mb-2 text-black">
-                  {translate({
-                    id: 'theme.docs.docHome.developerhint',
-                    message: 'Deploy Taichi programs in production.',
-                  })}
-                </div>
-                <div className="flex flex-col">
-                  <PackageIcon />
-                  <div className="flex justify-between items-center font-bold">
-                    <div className="text-h3">
-                      {translate({
-                        id: 'theme.text.aotdeployment',
-                        message: 'AOT Deployment',
-                      })}
-                    </div>
-                    <ArrowRight />
-                  </div>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <Link href="/api/" className="relative block h-24 rounded-sm bg-black desktop:mb-10 mb-6">
-            <div className="absolute right-0 flex items-center inset-y-0 overflow-hidden">
-              <AsyncImage src={AnotherWorldGif} />
-            </div>
-            <div className="flex relative flex-col h-full text-white justify-between px-4 py-3">
-              <div className="flex justify-between">
-                <div className="text-caption">
-                  {translate({
-                    id: 'theme.docs.docHome.slogan',
-                    message: 'Develop elegantly.',
-                  })}
-                </div>
-                <div className="text-caption hidden font-light desktop:block">
-                  Image by <span className="font-normal">peng-bo</span> in Voxel
-                  Challenge 2022
-                </div>
-              </div>
-                <div className="flex justify-between font-bold">
-                  <div className="text-h4 text-brand-cyan-gradients">
+              <div className="flex flex-col">
+                <UserIcon />
+                <div className="flex justify-between items-center font-bold">
+                  <div className="text-h3">
                     {translate({
-                      id: 'theme.docs.docHome.gotoapi',
-                      message: 'Taichi’s API reference here',
+                      id: 'theme.text.pythonuser',
+                      message: 'Python User',
+                    })}
+                  </div>
+                  <span>
+                    <ArrowRight />
+                  </span>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="relative flex-1 h-32 overflow-hidden brand-cyan-gradients rounded-sm mb-4">
+            <div className="absolute left-[181px] inset-y-0">
+              <DeveloperUserBg />
+            </div>
+            <Link
+              href="/docs/ndarray_android"
+              className="relative flex flex-col justify-between h-full px-5 pt-3 pb-4 text-black cursor-pointer hover:text-white"
+            >
+              <div className="text-caption  mb-2 text-black">
+                {translate({
+                  id: 'theme.docs.docHome.developerhint',
+                  message: 'Deploy Taichi programs in production.',
+                })}
+              </div>
+              <div className="flex flex-col">
+                <PackageIcon />
+                <div className="flex justify-between items-center font-bold">
+                  <div className="text-h3">
+                    {translate({
+                      id: 'theme.text.aotdeployment',
+                      message: 'AOT Deployment',
                     })}
                   </div>
                   <ArrowRight />
                 </div>
-            </div>
-          </Link>
-          <div className="space-y-4 md:space-y-5">
-            <div className="text-h2 font-bold">
-              {translate({
-                id: 'theme.docs.docHome.expandedviewofdocs',
-                message: 'Expanded View of Docs',
-              })}
-            </div>
-            <div className="grid grid-cols-1 desktop:grid-cols-3 gap-4 desktop:gap-5">
-              {props.sidebar.map((item: PropSidebarItem, index) => {
-                if (item.type === 'category' && item.items.length > 0) {
-                  return <ArticleCategoryCard key={index} category={item} />;
-                }
-                return null;
-              })}
-            </div>
+              </div>
+            </Link>
           </div>
         </div>
-      </DocPageLayout>
-    </Layout>
+        <Link href="/api/" className="relative block h-24 rounded-sm bg-black desktop:mb-10 mb-6">
+          <div className="absolute right-0 flex items-center inset-y-0 overflow-hidden">
+            <AsyncImage src={AnotherWorldGif} />
+          </div>
+          <div className="flex relative flex-col h-full text-white justify-between px-4 py-3">
+            <div className="flex justify-between">
+              <div className="text-caption">
+                {translate({
+                  id: 'theme.docs.docHome.slogan',
+                  message: 'Develop elegantly.',
+                })}
+              </div>
+              <div className="text-caption hidden font-light desktop:block">
+                Image by <span className="font-normal">peng-bo</span> in Voxel
+                Challenge 2022
+              </div>
+            </div>
+              <div className="flex justify-between font-bold">
+                <div className="text-h4 text-brand-cyan-gradients">
+                  {translate({
+                    id: 'theme.docs.docHome.gotoapi',
+                    message: 'Taichi’s API reference here',
+                  })}
+                </div>
+                <ArrowRight />
+              </div>
+          </div>
+        </Link>
+        <div className="space-y-4 md:space-y-5">
+          <div className="text-h2 font-bold">
+            {translate({
+              id: 'theme.docs.docHome.expandedviewofdocs',
+              message: 'Expanded View of Docs',
+            })}
+          </div>
+          <div className="grid grid-cols-1 desktop:grid-cols-3 gap-4 desktop:gap-5">
+            {props.sidebar.map((item: PropSidebarItem, index) => {
+              if (item.type === 'category' && item.items.length > 0) {
+                return <ArticleCategoryCard key={index} category={item} />;
+              }
+              return null;
+            })}
+          </div>
+        </div>
+      </div>
+    </DocPageLayout>
   );
 };
+
+export default (props) => {
+  return <Layout wrapperClassName={ThemeClassNames.wrapper.docsPages}
+  pageClassName={ThemeClassNames.page.docsDocPage}>
+    <LandingPageView {...props} />
+  </Layout>
+}
